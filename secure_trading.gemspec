98cba601b7175f68c4b7fdfc0868b9a21a4b136f
werleo <philip.chan@bitgamelabs.com>
2015-08-05 12:35:50 +0200

    add codeclimate-test-reporter

--- secure_trading.gemspec
+++ secure_trading.gemspec
@@ -26,4 +26,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'guard'
   spec.add_development_dependency 'guard-rspec'
   spec.add_development_dependency 'guard-rubocop'
+  spec.add_development_dependency "codeclimate-test-reporter", "~> 0.4"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
+if ENV['BUILDER'] == 'travis'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'secure_trading'
