9a764f0d6605f28492696a9e2992b51fead18233
werleo <philip.chan@bitgamelabs.com>
2015-08-06 18:05:24 +0200

    setup config for all except type: :no_config
    [#100634368]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,23 @@ end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'secure_trading'
+
+RSpec.configure do |config|
+  config.around(:each) do |test|
+    set_config if test.metadata[:type] != :no_config
+    test.run
+    reset_config if test.metadata[:type] != :no_config
+  end
+end
+
+def set_config(user = 'u', password = 'p', site_reference = 'sr')
+  SecureTrading.configure do |c|
+    c.user = user
+    c.password = password
+    c.site_reference = site_reference
+  end
+end
+
+def reset_config
+  SecureTrading.send('config=', nil)
+end
