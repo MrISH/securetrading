5f2e2b89f9e87fad27e0685b5591afd617a04c41
werleo <philip.chan@bitgamelabs.com>
2015-08-06 13:47:12 +0200

    add ConfigurationError and fail if required data is not present
    [#100634368]

--- lib/secure_trading/configuration.rb
+++ lib/secure_trading/configuration.rb
@@ -1,10 +1,22 @@
 module SecureTrading
   class Configuration
-    attr_accessor :user, :password, :sitereference
-    attr_writer :auth
+    class ConfigurationError < StandardError; end
+
+    attr_accessor :user, :password
+    attr_writer :site_reference
+
+    def site_reference
+      return @site_reference if @site_reference.present?
+      fail ConfigurationError, 'Site reference is required!'
+    end
 
     def auth
-      @auth ||= Base64.encode64 "#{user}:#{password}"
+      return if @auth.present?
+      if user.present? && password.present?
+        @auth ||= Base64.encode64("#{user}:#{password}")
+      else
+        fail ConfigurationError, 'User and password or auth are required!'
+      end
     end
   end
 end
