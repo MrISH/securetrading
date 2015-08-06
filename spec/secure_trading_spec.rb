71660203bd97235b942e85139d01329bce63d7f4
werleo <philip.chan@bitgamelabs.com>
2015-08-06 18:07:32 +0200

    set no_config where we are testing configuration
    [#100634368]

--- spec/secure_trading_spec.rb
+++ spec/secure_trading_spec.rb
@@ -9,7 +9,7 @@ describe SecureTrading do
 
   describe '#config' do
     context 'when not set' do
-      it 'returns new configuration' do
+      it 'returns new configuration', type: :no_config do
         new_config = SecureTrading::Configuration.new
         expect(SecureTrading::Configuration).to receive(:new) { new_config }
         expect(subject.config).to eq new_config
@@ -17,18 +17,14 @@ describe SecureTrading do
     end
 
     context 'when it is set' do
-      before do
-        described_class.configure { |c| c.user = 'a' }
-      end
-
       it 'contains set value' do
-        expect(subject.config.user).to eq 'a'
+        expect(subject.config.user).to eq 'u'
       end
     end
   end
 
   describe '#configure' do
-    it 'sets config' do
+    it 'sets config', type: :no_config do
       expect do
         described_class.configure { |c| c.user = 'a' }
       end.to change { described_class.config.user }.from(nil).to('a')
