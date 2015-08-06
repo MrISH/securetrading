50eb9fc9b118e0ef0d54aafd69137dc5541065c9
werleo <philip.chan@bitgamelabs.com>
2015-08-06 18:08:58 +0200

    efund to_xml spec
    [#100634368]

---
+++ spec/secure_trading/refund_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe SecureTrading::Refund do
+  describe '#to_xml' do
+    let(:options) { {} }
+    let(:refund) { described_class.new(100, 'parent', options) }
+    # rubocop:disable Metrics/LineLength
+    let(:expected_xml) do
+      "\n<requestblock version=\"3.67\">\n"\
+        "  <alias>u</alias>\n"\
+        "  <request type=\"REFUND\">\n"\
+        "    <operation>\n"\
+        "      <sitereference>sr</sitereference>\n"\
+        "      <accounttypedescription>ECOM</accounttypedescription>\n"\
+        "      <parenttransactionreference>parent</parenttransactionreference>\n"\
+        "    </operation>\n"\
+        "    <billing>\n"\
+        "      <amount>100</amount>\n"\
+        "    </billing>\n"\
+        "  </request>\n"\
+        "</requestblock>\n"
+    end
+    # rubocop:enable Metrics/LineLength
+
+    it 'returns xml representation of refund' do
+      expect(refund.to_xml).to eq expected_xml
+    end
+
+    context 'with account_type option' do
+      let(:options) { { account_type: 'CFT' } }
+
+      it 'returns xml with accounttypedescription tag' do
+        expect(refund.to_xml)
+          .to include('<accounttypedescription>CFT</accounttypedescription>')
+      end
+    end
+
+    context 'with merchant option' do
+      let(:options) { { merchant: { orderreference: 'orderId1' } } }
+      let(:merchant_tag) do
+        "    <merchant>\n"\
+        "      <orderreference>orderId1</orderreference>\n"\
+        '    </merchant>'
+      end
+
+      it 'returns xml representation of refund with merchant tag' do
+        expect(refund.to_xml).to include(merchant_tag)
+      end
+    end
+  end
+end
