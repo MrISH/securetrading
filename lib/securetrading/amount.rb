f88e725388d42951c53cbdb776c053cec3183c6a
werleo <philip.chan@bitgamelabs.com>
2015-08-24 11:53:58 +0200

    make proper xml for amount

--- lib/securetrading/amount.rb
+++ lib/securetrading/amount.rb
@@ -3,5 +3,11 @@ module Securetrading
     def value
       attributes_hash['__content__']
     end
+
+    def ox_xml
+      el = XmlDoc.new_element(xml_tag_name)
+      el['currencycode'] = currencycode
+      el << value.to_s
+    end
   end
 end
--- spec/securetrading/amount_spec.rb
+++ spec/securetrading/amount_spec.rb
@@ -3,11 +3,18 @@ require 'spec_helper'
 describe Securetrading::Amount do
   it { should respond_to(:value) }
 
-  let(:amount) { described_class.new(__content__: 1) }
+  let(:amount) { described_class.new(__content__: 1, currencycode: 'EUR') }
 
   describe '#value' do
     it 'returns value from __content__ attribute' do
       expect(amount.value).to eq(1)
+    end
+  end
+
+  describe '#ox_xml' do
+    it 'returns xml ox representation' do
+      expect(Ox.dump(amount.ox_xml))
+        .to eq("\n<amount currencycode=\"EUR\">1</amount>\n")
     end
   end
 end
