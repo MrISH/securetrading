e854600e968b7208976f3c9651d8d0b664aa2618
werleo <philip.chan@bitgamelabs.com>
2015-08-28 18:12:58 +0200

    fix amount xml generation when there was no currency code specified

--- lib/securetrading/amount.rb
+++ lib/securetrading/amount.rb
@@ -1,12 +1,16 @@
 module Securetrading
   class Amount < BaseModel
+    def currency_code
+      attributes_hash['currencycode']
+    end
+
     def value
       attributes_hash['content']
     end
 
     def ox_xml
       el = XmlDoc.new_element(xml_tag_name)
-      el['currencycode'] = currencycode
+      el['currencycode'] = currency_code if currency_code.present?
       el << value.to_s
     end
   end
--- spec/securetrading/amount_spec.rb
+++ spec/securetrading/amount_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Securetrading::Amount do
   it { should respond_to(:value) }
+  it { should respond_to(:currency_code) }
 
   let(:amount) { described_class.new(content: 1, currencycode: 'EUR') }
 
@@ -15,6 +16,15 @@ describe Securetrading::Amount do
     it 'returns xml ox representation' do
       expect(Ox.dump(amount.ox_xml))
         .to eq("\n<amount currencycode=\"EUR\">1</amount>\n")
+    end
+
+    context 'when there is no currencycode specified' do
+      let(:amount) { described_class.new(content: 1) }
+
+      it 'returns xml ox representation without currency' do
+        expect(Ox.dump(amount.ox_xml))
+          .to eq("\n<amount>1</amount>\n")
+      end
     end
   end
 end
