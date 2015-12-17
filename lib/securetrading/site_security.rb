283654d67cad74d6fd0a794f94041a75f61cbc27
werleo <philip.chan@bitgamelabs.com>
2015-12-17 14:35:14 +0100

    site_security with manual configuration

--- lib/securetrading/site_security.rb
+++ lib/securetrading/site_security.rb
@@ -1,20 +1,26 @@
 module Securetrading
   class SiteSecurity
-    def self.hash(fields)
-      Digest::SHA256.hexdigest(str_to_encode(fields))
+    def self.hash(fields, config_options = nil)
+      Digest::SHA256.hexdigest(str_to_encode(fields, config_options))
     end
 
-    def self.str_to_encode(fields)
+    def self.str_to_encode(fields, config_options = nil)
+      config = configuration(config_options)
       str = ''
-      fields.reverse_merge!(authmethod: Securetrading.config.auth_method)
-      [:currencyiso3a, :mainamount, :sitereference, :settlestatus,
-       :settleduedate, :authmethod, :paypaladdressoverride,
-       :strequiredfields, :version, :stprofile, :ruleidentifier,
-       :successfulurlredirect, :declinedurlredirect].each do |field|
+      fields.reverse_merge!(authmethod: config.auth_method)
+      %i(currencyiso3a mainamount sitereference settlestatus authmethod
+         settleduedate paypaladdressoverride strequiredfields version
+         stprofile ruleidentifier successfulurlredirect
+         declinedurlredirect).each do |field|
         str << fields[field].to_s
       end
-      str << Securetrading.config.site_security_password
+      str << config.site_security_password
     end
-    private_class_method :str_to_encode
+
+    def self.configuration(config_options)
+      return Securetrading.config unless config_options.present?
+      Configuration.new(config_options)
+    end
+    private_class_method :str_to_encode, :configuration
   end
 end
--- spec/securetrading/site_security_spec.rb
+++ spec/securetrading/site_security_spec.rb
@@ -22,4 +22,29 @@ describe Securetrading::SiteSecurity do
       end
     end
   end
+
+  describe '.configuration' do
+    let(:config_opts) { nil }
+    let(:configuration) do
+      described_class.send(:configuration, config_opts)
+    end
+    before do
+      Securetrading.configure { |c| c.auth_method = 'u' }
+    end
+    after { Securetrading.send('config=', 'nil') }
+
+    context 'when config_options are present' do
+      let(:config_opts) { { auth_method: 'from_opts' } }
+
+      it 'returns new Configuration initialized with config_options' do
+        expect(configuration.auth_method).to eq('from_opts')
+      end
+    end
+
+    context 'when config_options are not present' do
+      it 'returns config set by configuration' do
+        expect(configuration.auth_method).to eq('u')
+      end
+    end
+  end
 end
